ab4d33bdb5dc81fbac3cae70030d61f84015bbc1
SCN <supercodingninja@outlook.com>
2021-02-11 18:24:26 -0800

    Configured packages installed, and express handlebars.

--- server.js
+++ server.js
@@ -3,3 +3,12 @@ const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 
+// Configured packages installed
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(methodOverride("_method"));
+
+// Configured Express Handlebars
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(methodOverride("_method"));
