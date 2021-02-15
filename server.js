47129926ea733bb420d7d05084398c30fb3aba1f
SCN <supercodingninja@outlook.com>
2021-02-14 23:36:35 -0800

    Experimenting: working through an express tutorial.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 
-const routes = require("./routes/handlebars")
+const routes = require("./routes/handlers")
 
 const Port = process.env.Port || 9001;
 const app = express();
