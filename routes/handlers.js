ca1e3242afe0525cb90a4f81ecc2fa537c0c6fea
SCN <supercodingninja@outlook.com>
2021-02-16 21:39:28 -0800

    orm.selectAll (function(error, burgers) {...}

--- routes/handlers.js
+++ routes/handlers.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+const orm =require('../config/orm');
+
 // Get //
 router.get("/", function(req, res) {
-    res.json({
-        message: 'EDB!'
+    orm.selectAll (function(error, burgers) {
+        if (error) {
+            return res.status(501).json ({
+                message: 'Unable to query the database.'
+            });
+        }
+        res.render("index");
     });
 });
 
