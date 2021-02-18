49969319ce9ec8f6a6c53273876011acceefb646
SCN <supercodingninja@outlook.com>
2021-02-17 17:48:58 -0800

    Added functions; and corrected earlier functions.  I need to work on my cb's.

---
+++ config/ormUse.js
@@ -0,0 +1,42 @@
+const connection = require("./connection");
+
+const orm = {
+    selectAll: function (cb) {
+        connection.query("SELECT * FROM BigTimeEst_Burgers", function (err, data) {
+            if (err) cb(err, null);
+            cb(null, data);
+        });
+    },
+    selectAllBy: function(condition, value, cb) {
+        const sqlQuery = `SELECT * FROM BigTimeEst_Burgers WHERE ${condition } = ${value}`;
+        connection.query(sqlQuery, function (err, data) {
+            if (err) cb(err, null);
+            cb(null, data)
+        });
+    },
+    insertOne: function (burgerName, cb) {
+        const sqlQuery = `INSERT INTO BigTimeEst_Burgers(burger_name) VALUES('${burgerName}')`;
+        connection.query(sqlQuery, function (err, data) {
+            if (err) cb(err, null);
+            cb(null, data);
+        });
+    },
+
+    updateOne: function (condition, id, cb) {
+        const sqlQuery = `UPDATE BigTimeEst_Burgers SET is_favorite = ${condition} WHERE id = ${id}`;
+        connection.query(sqlQuery, function (err, data) {
+            if (err) cb(err, null);
+            cb(null, data)
+        });
+    },
+
+    deleteOne: function(id, cb) {
+        const sqlQuery = `DELETE FROM BigTimeEst_Burgers WHERE id = ${id}`;
+        connection.query(sqlQuery, function (err, data) {
+            if (err) cb(err, null);
+            cb(null, data)
+        });
+    }
+};
+
+module.exports = orm;
---
+++ db/seed.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO EDB(Burger_name, is_favorite)
+VALUES("Colosus Burger", false);
+
+INSERT INTO EDB(Burger_name, is_favorite)
+VALUES("MEGA-FAT Burger", false);
+
+INSERT INTO EDB(burger_name, is_favorite)
+VALUES("4-D-Hommies Burger", true);
