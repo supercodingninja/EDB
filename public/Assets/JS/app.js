ce3aa39e199a7ab6a44c3e36cde709218da195eb
SCN <supercodingninja@outlook.com>
2021-02-18 00:01:22 -0800

    VITAL NOTE TO SELF- REMEMBER!!!

---
+++ public/Assets/JS/app.js
@@ -0,0 +1,1 @@
+// IMPORTANT NOTE TO SELF: Go back to COMMITTING IMMEDIATELY after each function: so you do not lose your work, by accidental deletes, computer issues, etc.!  YOU'RE NEW TO THIS; and it's OKAY- who cares if more experience developers do not overly commit- YOU HAVE TO DO WHAT WORKS BEST FOR YOU; SO DO IT, AND DO IT GREAT!!! // 
