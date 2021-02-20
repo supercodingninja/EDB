f22a01e0e037862ec6f5a775dfd853f3f8f3bb01
SCN <supercodingninja@outlook.com>
2021-02-19 21:50:14 -0800

    Corrected previous functions; and working on how each burger will be deleted.

--- models/burger.js
+++ models/burger.js
@@ -4,18 +4,27 @@ const orm = require('../config/orm');
 let anyBurger = {
 
     selectall: function(cb) {
-        orm.selectAll('burgers', function(res) {
+        orm.selectAll('Burgers', function(res) {
             cb(res);
         });
     },
+    
     // This is how to get user's burger into a burger table.  Ref. https://docs.mongodb.com/drivers/node/usage-examples/insertOne/ "nsertOne() returns nothing. If you not specify one, this method returns a Promise that resolves to the result object when it completes." //
     insertOne: function(burgerName, cb) {
-        onm.insertOne('burgers', Burger_name, burgerName, cb)
-    }
-};
+        onm.insertOne('Burgers', Burger_name, burgerName, cb function(res) {
+            cb(res;)
+        });
+    },
 
-// Need to get rid of burgers: these will be the burgers "Devoured". Remember, you need a Boolean here. //
-burgerStatus: function(cb, burgerGoneBoolean, devouredBurgerID) {
+    // Need to get rid of burgers: these will be the burgers "Devoured". Remember, you need a Boolean here. //
+    burgerStatus: function(cb, burgerGoneBoolean, devouredBurgerID) {
+    
     // ORM!!!!  Use "updateOne," here (just like above). //
-    orm.updateOne('burgers', cb, burgerGoneBoolean, devouredBurgerID,)
-}
+    orm.updateOne('Burgers', cb, burgerGoneBoolean, devouredBurgerID, 'id', "Devoured", function(res) {
+        cb(res); // Remember to callback the response; and you do not need "req": "req is an object containing information about the HTTP request that raised the event. In response to req, you use res to send back the desired HTTP response."- Ref. https://stackoverflow.com/questions/4696283/what-are-res-and-req-parameters-in-express-functions //
+        });
+    },
+
+    // Need to delete the burger! //
+
+};
