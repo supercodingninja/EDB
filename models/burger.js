6ee0ebc85a4d27a8da2984fee409eae431210bf6
SCN <supercodingninja@outlook.com>
2021-02-19 22:09:41 -0800

    I noticed my IDE shown multiple errors; and I corrected all errors.

--- models/burger.js
+++ models/burger.js
@@ -5,32 +5,32 @@ const orm = require('../config/orm');
 
 let anyBurger = {
 
-    selectall: function(cb) {
+    selectall: function (cb) {
         
-        orm.selectAll('Burgers', function(res) {
+        orm.selectAll('Burgers', function (res) {
             
-            cb(res);
+            cb (res);
 
         });
     },
     
     // This is how to get user's burger into a burger table.  Ref. https://docs.mongodb.com/drivers/node/usage-examples/insertOne/ "nsertOne() returns nothing. If you not specify one, this method returns a Promise that resolves to the result object when it completes." //
-    insertOne: function(burgerName, cb) {
+    insertOne: function (burgerName, cb) {
         
-        onm.insertOne('Burgers', Burger_name, burgerName, cb function(res) {
+        onm.insertOne ('Burgers', Burger_name, burgerName, cb, function (res) {
             
-            cb(res;)
+            cb(res);
 
         });
     },
 
     // Need to get rid of burgers: these will be the burgers "Devoured". Remember, you need a Boolean here. //
-    burgerStatus: function(cb, burgerGoneBoolean, devouredBurgerID) {
+    burgerStatus: function (cb, burgerGoneBoolean, devouredBurgerID) {
     
     // ORM!!!!  Use "updateOne," here (just like above). //
-    orm.updateOne('Burgers', cb, burgerGoneBoolean, devouredBurgerID, 'id', "Devoured", function(res) {
+    orm.updateOne('Burgers', cb, burgerGoneBoolean, devouredBurgerID, 'id', "Devoured", function (res) {
         
-        cb(res); // Remember to callback the response; and you do not need "req": "req is an object containing information about the HTTP request that raised the event. In response to req, you use res to send back the desired HTTP response."- Ref. https://stackoverflow.com/questions/4696283/what-are-res-and-req-parameters-in-express-functions //
+        cb (res); // Remember to callback the response; and you do not need "req": "req is an object containing information about the HTTP request that raised the event. In response to req, you use res to send back the desired HTTP response."- Ref. https://stackoverflow.com/questions/4696283/what-are-res-and-req-parameters-in-express-functions //
         });
     },
 
