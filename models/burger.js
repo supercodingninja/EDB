07f4ec27daeabf7d6f9124b1c232e7a01238c99b
SCN <supercodingninja@outlook.com>
2021-02-19 21:22:29 -0800

    Working on insertOne and my update functions...

--- models/burger.js
+++ models/burger.js
@@ -1,16 +1,21 @@
 // Don't forget to require the ORM. //
 const orm = require('../config/orm');
 
-let burger = {
+let anyBurger = {
 
-    selectall: function(cb){
-        orm.selectAll('burgers', function(res){
+    selectall: function(cb) {
+        orm.selectAll('burgers', function(res) {
             cb(res);
         });
     },
     // This is how to get user's burger into a burger table.  Ref. https://docs.mongodb.com/drivers/node/usage-examples/insertOne/ "nsertOne() returns nothing. If you not specify one, this method returns a Promise that resolves to the result object when it completes." //
-    insertOne: function(burgerName, cb){
-        onm.insertOne('burgers', Burger_name)
+    insertOne: function(burgerName, cb) {
+        onm.insertOne('burgers', Burger_name, burgerName, cb)
     }
-
 };
+
+// Need to get rid of burgers: these will be the burgers "Devoured". Remember, you need a Boolean here. //
+burgerStatus: function(cb, burgerGoneBoolean, devouredBurgerID) {
+    // ORM!!!!  Use "updateOne," here (just like above). //
+    orm.updateOne('burgers', cb, burgerGoneBoolean, devouredBurgerID,)
+}
