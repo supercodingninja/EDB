88696d0398550565b936919a1707b0cd90f06877
SCN <supercodingninja@outlook.com>
2021-02-19 22:04:35 -0800

    I cleaned my functions up (gave more space and careful comments); and I added my "deleteOne" function.

--- models/burger.js
+++ models/burger.js
@@ -1,18 +1,26 @@
 // Don't forget to require the ORM. //
+const { deleteOne } = require('../config/orm');
+
 const orm = require('../config/orm');
 
 let anyBurger = {
 
     selectall: function(cb) {
+        
         orm.selectAll('Burgers', function(res) {
+            
             cb(res);
+
         });
     },
     
     // This is how to get user's burger into a burger table.  Ref. https://docs.mongodb.com/drivers/node/usage-examples/insertOne/ "nsertOne() returns nothing. If you not specify one, this method returns a Promise that resolves to the result object when it completes." //
     insertOne: function(burgerName, cb) {
+        
         onm.insertOne('Burgers', Burger_name, burgerName, cb function(res) {
+            
             cb(res;)
+
         });
     },
 
@@ -21,10 +29,20 @@ let anyBurger = {
     
     // ORM!!!!  Use "updateOne," here (just like above). //
     orm.updateOne('Burgers', cb, burgerGoneBoolean, devouredBurgerID, 'id', "Devoured", function(res) {
+        
         cb(res); // Remember to callback the response; and you do not need "req": "req is an object containing information about the HTTP request that raised the event. In response to req, you use res to send back the desired HTTP response."- Ref. https://stackoverflow.com/questions/4696283/what-are-res-and-req-parameters-in-express-functions //
         });
     },
 
     // Need to delete the burger! //
+    deleteOne: function(cb, id) {
 
+        orm.deleteOne('id', id, 'Burgers', function(res) {
+            
+            cb(res);
+
+        });
+    }
 };
+
+module.exports = anyBurger;
