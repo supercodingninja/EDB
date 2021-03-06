a9d20db66cf39e082a773b1b69372bc7270a043d
SCN <supercodingninja@outlook.com>
2021-03-05 23:11:26 -0800

    Uncommented my see.sql file: I'm trying to see if my data will populate.

--- db/seed.sql
+++ db/seed.sql
@@ -2,11 +2,11 @@ USE BigTimeEst_Burgers_db;
 
 INSERT INTO BigTimeEst_Burgers (Burger_name) VALUES ("Colosus Burger"), ("MEGA-FAT Burger"), ("4-D-Hommies Burger");
 
--- INSERT INTO BigTimeEst_Burgers(Burger_name, is_favorite)
--- VALUES("Colosus Burger", false);
+INSERT INTO BigTimeEst_Burgers(Burger_name, is_favorite)
+VALUES("Colosus Burger", false);
 
--- INSERT INTO BigTimeEst_Burgers(Burger_name, is_favorite)
--- VALUES("MEGA-FAT Burger", false);
+INSERT INTO BigTimeEst_Burgers(Burger_name, is_favorite)
+VALUES("MEGA-FAT Burger", false);
 
--- INSERT INTO BigTimeEst_Burgers(burger_name, is_favorite)
--- VALUES("4-D-Hommies Burger", true);
+INSERT INTO BigTimeEst_Burgers(burger_name, is_favorite)
+VALUES("4-D-Hommies Burger", true);
