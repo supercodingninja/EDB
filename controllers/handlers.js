b3116a349977788c3279050e30b09f37b819f59d
SCN <supercodingninja@outlook.com>
2021-03-07 21:07:43 -0800

    Created all routes, and set up logic within those routes, where required.

--- controllers/handlers.js
+++ controllers/handlers.js
@@ -1,15 +1,53 @@
 const express = require("express");
 const router = express.Router();
 
+// Import the model (burger.js) to use its database functions. //
 const burger =require('../models/burger');
 
-// Get //
+
+// Routes, and set up logic within those routes, where required. //
+// Get (C. R. U. D.: CREATE) //
 router.get("/", function(req, res) {
     burger.selectAll (function(data) {
        
+        var hbsObject = {
 
-        res.render('index', {burgers:data});
+            burgers: data
+        };
+
+        console.log(hbsObject);
+
+        res.render("index", hbsObject);
     });
 });
+
+
+// Post (C. R. U. D.: READ) //
+router.post("/api/burgers", function(req, res) {
+    
+    burger.create(
+        
+        [
+            "name",
+            
+            "favorite"
+        ], 
+
+        [
+            req.body.name,
+            
+            req.body.favorite
+        ],
+        
+        function(result) {
+      
+            // Send back the ID of the new burger. //
+            res.json({ id: result.insertId });
+        }
+    );
+});
+
+
+
 
 module.exports = router;
